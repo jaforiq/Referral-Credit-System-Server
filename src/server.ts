c420a17b3433aa73b34005eb5c3e9bcda737f140
jaforiq <jaforiqbal5593@gmail.com>
2025-10-26 00:53:44 +0600

    localhost changed to deployed url

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 app.use(helmet());
 
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL,
   credentials: true,
   optionsSuccessStatus: 200,
 };
